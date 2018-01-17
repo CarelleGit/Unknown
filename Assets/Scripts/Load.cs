c44d1bbedaf6da60fe158645166b589a4b2966b1
Kimberly Macfarlane <s179062@students.aie.edu.au>
2018-01-17 11:29:12 -0800

    Main menu created

---
+++ Assets/Scripts/Load.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Load : MonoBehaviour {
+
+	public void Level(int idx)
+    {
+        SceneManager.LoadScene(idx);
+    }
+}
