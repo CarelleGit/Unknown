a91da07abefad4c3f145f38e764192f15b1ff159
Kimberly <steamtik@gmail.com>
2018-01-17 18:21:39 -0800

    Got player working on the first level. Jump needs some work

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody2D rb2;
+    public float jump;
+   public GameObject ground;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+
+        Vector2 move = new Vector2(horizontal, 0);
+        Vector2 desiredVelocity = move * speed * Time.deltaTime;
+        rb2.velocity = new Vector2(desiredVelocity.x, rb2.velocity.y);
+        if(Input.GetButtonDown("Jump"))
+        {
+            if (ground)
+            {
+                rb2.AddForce(Vector2.up * jump);
+            }
+        }
+	}
+}
